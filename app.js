08907dccb4d9d7bd1d71955f5f7becab30a8b736
micheledidonna01 <micheledidonna337@gmail.com>
2025-05-08 16:15:21 +0200

    feat: Milestone 1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 let port = 3002;
-const posts = require('./data/posts.js');
+
 const errorsHandler = require('./middlewares/errorsHandler.js');
 const notFound = require('./middlewares/notFound.js');
 
