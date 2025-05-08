bd205974207667c57c4c9fd31832b095b2567ccc
micheledidonna01 <micheledidonna337@gmail.com>
2025-05-08 16:21:32 +0200

    feat: Milestone 2

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -3,6 +3,13 @@ const connection = require('../data/db.js');
 
 //index
 function index(req, res) {
+
+    const sql = 'SELECT * FROM posts';
+
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({error: 'Database query failed'});
+        res.json(results);
+    });
     // pippo.get();
     // let filterPosts = posts;
     // let filterPostsIndex;
